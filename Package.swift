6675054478880b7f1a6ff13247e24232d980ff87
Mikael Bartlett <mikael.bartlett@wagestream.com>
2024-09-17 07:11:32 +0200

    Add Linux binary dependency

--- Package.swift
+++ Package.swift
@@ -2,6 +2,12 @@
 
 import PackageDescription
 
+#if os(macOS)
+    let dependencies: [Dependency] = [.target(name: "SwiftLintBinaryMacOS")]
+#else
+    let dependencies: [Dependency] = [.target(name: "SwiftLintBinaryLinux")]
+#endif
+
 let package = Package(
     name: "SwiftLintPlugins",
     platforms: [
@@ -9,23 +15,30 @@ let package = Package(
     ],
     products: [
         .plugin(name: "SwiftLintBuildToolPlugin", targets: ["SwiftLintBuildToolPlugin"]),
-        .plugin(name: "SwiftLintCommandPlugin", targets: ["SwiftLintCommandPlugin"])
+        .plugin(name: "SwiftLintCommandPlugin", targets: ["SwiftLintCommandPlugin"]),
     ],
     targets: [
         .plugin(
             name: "SwiftLintBuildToolPlugin",
             capability: .buildTool(),
-            dependencies: [.target(name: "SwiftLintBinary")]
+            dependencies: [.target(name: "SwiftLintBinaryMacOS")]
         ),
         .plugin(
             name: "SwiftLintCommandPlugin",
-            capability: .command(intent: .custom(verb: "swiftlint", description: "SwiftLint Command Plugin")),
-            dependencies: [.target(name: "SwiftLintBinary")]
+            capability: .command(
+                intent: .custom(verb: "swiftlint", description: "SwiftLint Command Plugin")),
+            dependencies: [.target(name: "SwiftLintBinaryMacOS")]
         ),
         .binaryTarget(
-            name: "SwiftLintBinary",
-                url: "https://github.com/realm/SwiftLint/releases/download/0.57.0/SwiftLintBinary-macos.artifactbundle.zip",
-                checksum: "a1bbafe57538077f3abe4cfb004b0464dcd87e8c23611a2153c675574b858b3a"
-        )
+            name: "SwiftLintBinaryMacOS",
+            url:
+                "https://github.com/realm/SwiftLint/releases/download/0.57.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "a1bbafe57538077f3abe4cfb004b0464dcd87e8c23611a2153c675574b858b3a"
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinaryMacOS",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.57.0/swiftlint_linux.zip",
+            checksum: "│def1803e705c738c97fbc275c34e7a2e0278944776a8b9c56ebe8c444ea57407"
+        ),
     ]
 )
